9d00e46c29710d932baa1448c121872f8cedc20c
Lucas Dietrich <ld.adecy@gmail.com>
2022-11-11 22:48:26 +0100

    List few devices information

--- samples/devices.py
+++ samples/devices.py
@@ -7,15 +7,21 @@
 from pprint import pprint
 from caniot.controller import Controller
 import json
+import time
 
 qemu = False
-qemu = True
 
 ip = "192.0.2.1" if qemu else "192.168.10.240"
 
 with Controller(ip) as ctrl:
     devices = ctrl.get_devices()
     stats = ctrl.get_ha_stats()
+
+now = time.time()
+for dev in devices:
+    diff = 3600*2
+    last_seen = now - dev["endpoints"][0]["last_event"]["timestamp"] - diff
+    print(f"{dev['addr_type']} {dev['addr_medium']} {dev['addr_repr']} : -{last_seen:.0f}s")
 
 with open("tmp/ha_devices.json", "w") as f:
    json.dump(devices, f, indent=4)
